07abc3365b49aa93b0c658d5c7ebcf18e5ce0b15
malek <boulahdourabdelmalek@gmail.com>
2025-06-26 17:51:22 +0100

    changed the templates format

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['website', 'items', 'dashboard'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
